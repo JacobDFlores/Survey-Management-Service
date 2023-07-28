4cf680d50bf98dcb8eb48abe7e66785be4120010
Mardyyy <martinapopot@gmail.com>
2023-07-27 21:32:35 -0500

    Added working post and delete routes in surveyRoutes.js. Added displayed and working delete button on profiles page.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -33,6 +33,7 @@ router.get('/profile', withAuth, async (req, res) => {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ['password'] },
+      include: [{ model: Surveys }],
     });
 
     const user = userData.get({ plain: true });
